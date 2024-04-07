6ccc38f0cefd069defb8f8dca11ba10cc4231867
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-04-07 11:36:09 +0800

    .

--- notes/compile.sh
+++ notes/compile.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-cd Chapter_1_事件与概率
+# cd Chapter_1_事件与概率
+# xelatex -output-directory=. ./main.tex
+# bibtex ./main.aux
+# xelatex -output-directory=. ./main.tex
+# xelatex -output-directory=. ./main.tex
+# cd ..
+
+cd Chapter_2_随机变量
 xelatex -output-directory=. ./main.tex
 bibtex ./main.aux
 xelatex -output-directory=. ./main.tex
 xelatex -output-directory=. ./main.tex
+cd ..
